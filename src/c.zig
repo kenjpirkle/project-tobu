b463b06b45e2a94508dbaa028903b725c80c5413
Ken Pirkle <kenjpirkle@gmail.com>
2020-07-15 19:52:41 +0100

    Remove sqlite from c.zig

--- src/c.zig
+++ src/c.zig
@@ -1,8 +1,7 @@
 pub usingnamespace @cImport({
     @cDefine("_NO_CRT_STDIO_INLINE", "1");
     @cInclude("stdio.h");
+    @cInclude("freetype.h");
     @cInclude("glad.h");
     @cInclude("glfw3.h");
-    @cInclude("freetype.h");
-    @cInclude("sqlite3.h");
 });

2833687ee501997419f7b29ba599367bf4925f30
Ken Pirkle <kenjpirkle@gmail.com>
2020-07-19 02:33:22 +0100

    Adjust block_vertex.glsl for testing

--- shaders/block_vertex.glsl
+++ shaders/block_vertex.glsl
@@ -1,12 +1,48 @@
 #version 460 core
 
-layout (location = 0) in vec3 position;
-
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 out vec4 colour;
+
+const vec3 vertices[] = {
+    // front face
+    vec3(0, 0, 1),
+    vec3(1,  0, 1),
+    vec3(0, 1,  1),
+    vec3(1,  1,  1),
+
+    // back face
+    vec3(1,  0, 0),
+    vec3(0, 0, 0),
+    vec3(1,  1,  0),
+    vec3(0, 1,  0),
+
+    // left face
+    vec3(0, 0, 0),
+    vec3(0, 0, 1),
+    vec3(0, 1,  0),
+    vec3(0, 1,  1),
+
+    // right face
+    vec3(1,  0, 1),
+    vec3(1,  0, 0),
+    vec3(1,  1,  1),
+    vec3(1,  1,  0),
+
+    // top face
+    vec3(0, 1,  1),
+    vec3(1,  1,  1),
+    vec3(0, 1,  0),
+    vec3(1,  1,  0),
+
+    // bottom face
+    vec3(0, 0, 0),
+    vec3(1,  0, 0),
+    vec3(0, 0, 1),
+    vec3(1,  0, 1)
+};
 
 const vec4 colours[] = {
     // Stone
@@ -21,21 +57,22 @@ const vec4 colours[] = {
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(position, 1.0);
+	gl_Position = projection * view * model * vec4(vertices[gl_VertexID], 1.0);
     const uint face = gl_VertexID / 6;
-    if (face == 0) {
-        colour = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if (face == 1) {
-        colour = vec4(0.0, 1.0, 0.0, 1.0);
-    } else if (face == 2) {
-        colour = vec4(0.0, 0.0, 1.0, 1.0);
-    } else if (face == 3) {
-        colour = vec4(1.0, 1.0, 0.0, 1.0);
-    } else if (face == 4) {
-        colour = vec4(1.0, 0.0, 1.0, 1.0);
-    } else if (face == 5) {
-        colour = vec4(0.0, 1.0, 1.0, 1.0);
-    } else {
-        colour = vec4(0.0, 0.0, 0.0, 1.0);
-    }
+    colour = vec4(0.1 * gl_Position.x, 0.1 * gl_Position.z, 0.1 * gl_Position.y, 1.0);
+    // if (face == 0) {
+    //     colour = vec4(1.0, 0.0, 0.0, 1.0);
+    // } else if (face == 1) {
+    //     colour = vec4(0.0, 1.0, 0.0, 1.0);
+    // } else if (face == 2) {
+    //     colour = vec4(0.0, 0.0, 1.0, 1.0);
+    // } else if (face == 3) {
+    //     colour = vec4(1.0, 1.0, 0.0, 1.0);
+    // } else if (face == 4) {
+    //     colour = vec4(1.0, 0.0, 1.0, 1.0);
+    // } else if (face == 5) {
+    //     colour = vec4(0.0, 1.0, 1.0, 1.0);
+    // } else {
+    //     colour = vec4(0.0, 0.0, 0.0, 1.0);
+    // }
 }

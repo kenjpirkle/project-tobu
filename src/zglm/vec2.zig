5a32278391d366be6a60d056c34eadf40098d6c2
Ken Pirkle <kenjpirkle@gmail.com>
2020-07-28 02:00:41 +0100

    Add Vec2 type

---
+++ src/zglm/vec2.zig
@@ -0,0 +1,6 @@
+pub const Vec2 = packed struct {
+    const Self = @This();
+
+    x: f32,
+    y: f32,
+};
--- src/zglm/zglm.zig
+++ src/zglm/zglm.zig
@@ -1,3 +1,4 @@
+pub usingnamespace @import("vec2.zig");
 pub usingnamespace @import("vec3.zig");
 pub usingnamespace @import("vec4.zig");
 pub usingnamespace @import("mat4.zig");
